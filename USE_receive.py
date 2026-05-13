2719bd6fc1d828ee82fa5af150cbef2f5312b8e9
Mangostin <Arikui57@outlook.jp>
2026-05-13 20:26:52 +0900

    ファイルの整理
    USERが使うファイルは
    USER_receiveとthrowにしました。
    settingとdevicesを追加しました。
    全体の変数…サンプリング周波数や入力
    はsettingで決めます。
    devicesはデバイスに関するメソッドです

---
+++ USE_receive.py
@@ -0,0 +1,5 @@
+
+
+
+from setting import splrat
+from setting import code
---
+++ USE_throw.py
@@ -0,0 +1,11 @@
+import sounddevice as sd
+import fnmatch
+
+from setting import splrat
+from setting import code
+
+import wave
+import struct
+import math
+
+
