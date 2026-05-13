69d8fea57be01263e153c1524b50b02169842868
Mangostin <Arikui57@outlook.jp>
2026-05-13 20:34:39 +0900

    digit_analog.settingの作成
    settingは、設定です。見てください
    digitのほうは、wavfileを作成する関数を追加しました。ただ追加しただけで、調整してません。
    それから、設定の作成に伴って変数名をいくつか変更しました。

--- digit_to_analog.py
+++ digit_to_analog.py
@@ -1,5 +1,8 @@
 
-
+import time from time
+import wave
+import struct
+import math
 import numpy as np
 from test_encode import codes
 strcode=codes[0]
@@ -20,15 +23,31 @@ def ls_to_code(ls):#リストを受け取る。　[227, 129, 147, 97, 98, 35, 91
     code_list = [hex(num)[2:] for num in ls]#10進数を16進数に変換してリストにする
     return ''.join(code_list)#リストを文字列にする
 
-def ls_to_wave(ls,spl_rag):#リストを受け取る。　[227, 129, 147, 97, 98, 35, 91, 91, 58, 47, 112, 59, 45, 64]のような
+def ls_to_wave(ls,splpeat):#リストを受け取る。　[227, 129, 147, 97, 98, 35, 91, 91, 58, 47, 112, 59, 45, 64]のような
     if type(ls) is not np.ndarray:#入力がnumpy配列でない場合はエラーを出す
         raise TypeError("Input must be a numpy array")
     ls/=255.0*2-1#0~255の値を-1~1の範囲に変換する
-    rls=np.repeat(ls, spl_rag)#リストを繰り返して波形を作る
+    rls=np.repeat(ls, splpeat)#リストを繰り返して波形を作る
     return rls
 
-def wave_to_ls(wave,spl_rag):#波形を受け取る.
+def wave_to_ls(wave,splpeat):#波形を受け取る.
     if type(wave) is not np.ndarray:#入力がnumpy配列でない場合はエラーを出す
         raise TypeError("Input must be a numpy array")
-    ls=np.array([int((num+1)/2*255) for num in wave[::spl_rag]])#波形を0~255の値に変換してリストにする
+    ls=np.array([int((num+1)/2*255) for num in wave[::splpeat]])#波形を0~255の値に変換してリストにする
     return ls
+
+def makewav():
+    filename = "test.wav"
+    sample_rate = 44100
+    duration = 1.0
+    frequency = 440.0
+
+    with wave.open(filename, "w") as wf:
+        wf.setnchannels(1)          # モノラル
+        wf.setsampwidth(2)          # 16bit
+        wf.setframerate(sample_rate)
+
+        for i in range(int(sample_rate * duration)):
+            value = int(32767 * math.sin(2 * math.pi * frequency * i / sample_rate))
+            data = struct.pack("<h", value)
+            wf.writeframesraw(data)
--- setting.py
+++ setting.py
@@ -1,1 +1,24 @@
 
+"""low level setting for this project
+"""
+splrat=44100#サンプリングレート
+code='utf-8'#文字コード
+splpeat=5#いくつのサンプルで一つの数字を表すか。少なければ速度は速いがノイズに弱く、多ければ速度は遅いがノイズに強い
+
+
+"""high level setting for this project
+"""
+inputdex=None#音声入力デバイスのインデックス。Noneの場合はHeadset Microphoneを探す。
+otputdex=None#音声出力デバイスのインデックス。Noneの場合はHeadphoneを探す。
+datasamples={"hex016": list(range(16))
+             ,"hex": [hex(i) for i in range(1,16)]
+             ,"hexmaixn": [range(1,16,8)]*6}#[0, 51, 102, 153, 204, 255]のような。データのサンプルを格納するリスト
+noisesample=datasamples["hex016"]+datasamples["hex"]#ノイズの大きさを測定するために使用する。
+
+from devices import there_is_input 
+if inputdex is None:
+    inputdex=there_is_input()
+
+from devices import there_is_output
+if otputdex is None:
+    otputdex=there_is_output()
