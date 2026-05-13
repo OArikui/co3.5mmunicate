20584b7075a5628f87654de9b6141242246f3179
Mangostin <Arikui57@outlook.jp>
2026-05-13 17:11:35 +0900

    文字のバイトから文字を再現する実験

--- test_encode.py
+++ test_encode.py
@@ -1,13 +1,25 @@
 codes=['utf-8', 'utf-16', 'ASCII']
 strcode=codes[0]
-#testdata for convert analog
-d1='*あいう' 
-d2="abcd"
-unid1=d1.encode(strcode)
-unid2=d2.encode(strcode)
-print(unid1,unid2)
-fusion=unid1+unid2
-print(fusion.decode(strcode))
-print(str(unid1))
-fusionst=str(unid2)+str(unid1)
-print(fusionst)
+#copilot saids unicode is based on this method 
+"""
+『こ』=U+3053
+① まず U+3053 を2進数にする
+U+3053 = 0b0011 0000 0101 0011
+② UTF-8 の3バイト形式に当てはめる
+1110xxxx 10xxxxxx 10xxxxxx
+③ 結果が E3 81 93 になる
+1110 0011 → E3
+10 000001 → 81
+10 0011   → 93"""
+
+"""コードを自分で作成して、文字列に変換する実験.
+koは、3.5mmから取得したanalogをデジタル変換したものを16進数で表したもの.を模している"""
+
+ko=["e3","81","93"]
+kostr=""
+for i in ko:
+    kostr+=i
+print(kostr)
+testbyte=bytes.fromhex(kostr)
+print(testbyte)
+print(testbyte.decode(strcode))
